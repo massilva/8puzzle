71808b2a91c68f21e587e5d451cb23c6bd0348b9
Victor dos Santos <vitocr_santos@hotmail.com>
2013-12-14 17:00:15 -0300

    Alterando forma de exibição do GUI

--- src/main/PuzzleGui.java
+++ src/main/PuzzleGui.java
@@ -2,6 +2,7 @@ package main;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -9,6 +10,12 @@ import javax.swing.JButton;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.text.ParseException;
+
+import javax.swing.SwingConstants;
+import javax.swing.text.MaskFormatter;
 
 @SuppressWarnings("serial")
 public class PuzzleGui extends JFrame {
@@ -36,8 +43,9 @@ public class PuzzleGui extends JFrame {
 	
 	/**
 	 * Create the frame.
+	 * @throws ParseException 
 	 */
-	public PuzzleGui(Puzzle a){
+	public PuzzleGui(Puzzle a) throws ParseException{
 		
 		myAgent = a;
 		
@@ -47,47 +55,78 @@ public class PuzzleGui extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		input0 = new JTextField();
+		input0 = new JFormattedTextField(new MaskFormatter("#"));
+		input0.setHorizontalAlignment(SwingConstants.CENTER);
 		input0.setBounds(0, 10, 50, 50);
 		contentPane.add(input0);
-		input0.setColumns(10);
+		input0.setColumns(1);
 		
-		input1 = new JTextField();
+		input1 = new JFormattedTextField(new MaskFormatter("#"));
+		input1.setHorizontalAlignment(SwingConstants.CENTER);
 		input1.setColumns(10);
 		input1.setBounds(50, 10, 50, 50);
 		contentPane.add(input1);
 		
-		input2 = new JTextField();
+		input2 = new JFormattedTextField(new MaskFormatter("#"));
+		input2.setHorizontalAlignment(SwingConstants.CENTER);
+		input2.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent arg0) {
+//				input3.transferFocus();
+				input3.grabFocus();
+				input3.requestFocus();
+			}
+		});
 		input2.setColumns(10);
 		input2.setBounds(100, 10, 50, 50);
 		contentPane.add(input2);
 		
-		input3 = new JTextField();
+		input3 = new JFormattedTextField(new MaskFormatter("#"));
+		input3.setHorizontalAlignment(SwingConstants.CENTER);
 		input3.setColumns(10);
 		input3.setBounds(0, 60, 50, 50);
 		contentPane.add(input3);
 		
-		input4 = new JTextField();
+		input4 = new JFormattedTextField(new MaskFormatter("#"));
+		input4.setHorizontalAlignment(SwingConstants.CENTER);
 		input4.setColumns(10);
 		input4.setBounds(50, 60, 50, 50);
 		contentPane.add(input4);
 		
-		input5 = new JTextField();
+		input5 = new JFormattedTextField(new MaskFormatter("#"));
+		input5.setHorizontalAlignment(SwingConstants.CENTER);
+		input5.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent e) {
+				input6.grabFocus();
+				input6.requestFocus();
+			}
+		});
 		input5.setColumns(10);
 		input5.setBounds(100, 60, 50, 50);
 		contentPane.add(input5);
 		
-		input6 = new JTextField();
+		input6 = new JFormattedTextField(new MaskFormatter("#"));
+		input6.setHorizontalAlignment(SwingConstants.CENTER);
 		input6.setColumns(10);
 		input6.setBounds(0, 110, 50, 50);
 		contentPane.add(input6);
 		
-		input7 = new JTextField();
+		input7 = new JFormattedTextField(new MaskFormatter("#"));
+		input7.setHorizontalAlignment(SwingConstants.CENTER);
 		input7.setColumns(10);
 		input7.setBounds(50, 110, 50, 50);
 		contentPane.add(input7);
 		
-		input8 = new JTextField();
+		input8 = new JFormattedTextField(new MaskFormatter("#"));
+		input8.setHorizontalAlignment(SwingConstants.CENTER);
+		input8.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent e) {
+				input0.grabFocus();
+				input0.requestFocus();
+			}
+		});
 		input8.setColumns(10);
 		input8.setBounds(100, 110, 50, 50);
 		contentPane.add(input8);
@@ -119,46 +158,64 @@ public class PuzzleGui extends JFrame {
 		contentPane.add(StartGame);			
 		
 		output1 = new JTextField();
+		output1.setHorizontalAlignment(SwingConstants.CENTER);
+		output1.setEditable(false);
 		output1.setColumns(10);
 		output1.setBounds(214, 10, 50, 50);
 		contentPane.add(output1);
 		
 		output2 = new JTextField();
+		output2.setHorizontalAlignment(SwingConstants.CENTER);
+		output2.setEditable(false);
 		output2.setColumns(10);
 		output2.setBounds(264, 10, 50, 50);
 		contentPane.add(output2);
 		
 		output0 = new JTextField();
+		output0.setHorizontalAlignment(SwingConstants.CENTER);
+		output0.setEditable(false);
 		output0.setColumns(10);
 		output0.setBounds(164, 10, 50, 50);
 		contentPane.add(output0);
 		
 		output3 = new JTextField();
+		output3.setHorizontalAlignment(SwingConstants.CENTER);
+		output3.setEditable(false);
 		output3.setColumns(10);
 		output3.setBounds(164, 60, 50, 50);
 		contentPane.add(output3);
 		
 		output4 = new JTextField();
+		output4.setHorizontalAlignment(SwingConstants.CENTER);
+		output4.setEditable(false);
 		output4.setColumns(10);
 		output4.setBounds(214, 60, 50, 50);
 		contentPane.add(output4);
 		
 		output5 = new JTextField();
+		output5.setHorizontalAlignment(SwingConstants.CENTER);
+		output5.setEditable(false);
 		output5.setColumns(10);
 		output5.setBounds(264, 60, 50, 50);
 		contentPane.add(output5);
 		
 		output6 = new JTextField();
+		output6.setHorizontalAlignment(SwingConstants.CENTER);
+		output6.setEditable(false);
 		output6.setColumns(10);
 		output6.setBounds(164, 110, 50, 50);
 		contentPane.add(output6);
 		
 		output7 = new JTextField();
+		output7.setHorizontalAlignment(SwingConstants.CENTER);
+		output7.setEditable(false);
 		output7.setColumns(10);
 		output7.setBounds(214, 110, 50, 50);
 		contentPane.add(output7);
 		
 		output8 = new JTextField();
+		output8.setHorizontalAlignment(SwingConstants.CENTER);
+		output8.setEditable(false);
 		output8.setColumns(10);
 		output8.setBounds(264, 110, 50, 50);
 		contentPane.add(output8);
