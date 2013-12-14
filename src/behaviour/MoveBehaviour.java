0fe70df04a972cd8f125a9cabdf6325d8d6f8f86
Victor dos Santos <vitocr_santos@hotmail.com>
2013-12-14 19:41:29 -0300

    Adicionando a movimentação por etapas

--- src/behaviour/MoveBehaviour.java
+++ src/behaviour/MoveBehaviour.java
@@ -1,5 +1,12 @@
 package behaviour;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
+import javax.swing.JButton;
+
 import main.Puzzle;
 import model.Node;
 import jade.core.behaviours.OneShotBehaviour;
@@ -8,18 +15,88 @@ public class MoveBehaviour extends OneShotBehaviour{
 
 	private static final long serialVersionUID = 1L;
 	private Node nSaida = null;
+	private ArrayList<Node> path = new ArrayList<Node>();
 	private Puzzle agent = null;
 	private String lS = "----------------------------------------------------";
-		
+	private ListIterator<Node> li = null;
+	private boolean prev = false, next = false; 
+	
 	public MoveBehaviour(Puzzle agent, Node nSaida){
 		this.nSaida = nSaida;
 		this.agent = (Puzzle) agent;
+		this.li = (ListIterator<Node>) path.listIterator();
 	}
 	
 	@Override
 	public void action() {
-		reconstructSuccess(nSaida);
-		agent.gui.setOutput(nSaida.getState());
+		createPath(nSaida);
+//		reconstructSuccess(nSaida);
+		JButton btnSolve = agent.gui.getBtnSolve();
+		btnSolve.setVisible(true);
+		JButton btnStep = agent.gui.getBtnStep();
+		btnStep.setVisible(true);
+		JButton btnStep_ = agent.gui.getBtnStep_();
+		btnStep_.setVisible(true);
+		
+		btnSolve.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				Iterator<Node> it = path.iterator();
+				while(it.hasNext()){
+					Node atual = it.next();
+					agent.gui.setOutput(atual.getState());	
+				}
+			}
+		});
+		
+		this.li = (ListIterator<Node>) path.listIterator();
+		printNext();
+		
+		btnStep.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				printNext();
+			}
+		});
+		
+		btnStep_.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				printPrevious();
+			}
+		});
+	}
+	
+	public void printNext(){
+		if(this.li.hasNext()){
+			if(prev)
+			{
+				this.li.next();
+				prev = false;
+			}
+			Node atual = this.li.next();
+			agent.gui.setOutput(atual.getState());
+			next = true;
+		}
+	}
+
+	public void printPrevious(){
+		if(this.li.hasPrevious()){
+			if(next){
+				this.li.previous();
+				this.next = false;
+			}
+			Node atual = this.li.previous();
+			agent.gui.setOutput(atual.getState());
+			prev = true;
+		}
+	}
+	public void createPath(Node a)
+	{
+		if (a.getParent() != null)
+			createPath(a.getParent());
+		
+		this.path.add(a);
 	}
 	
 	/**
